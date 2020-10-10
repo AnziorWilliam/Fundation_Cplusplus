4c61d359c9c7a968118765e73b821bce6def3c8b
HenryOrdin <HenryOrdin@163.com>
2020-10-10 18:19:22 +0800

    字符串替换

---
+++ Symbol_replace/Symbol_replace.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void Replace_symbol(string& str);
+int main()
+{
+    string Originary_str = "i have a dream";
+    Replace_symbol(Originary_str);
+    cout << Originary_str << endl;
+    return 0;
+}
+
+void Replace_symbol(string& str)
+{
+    auto StrEnd = str.end();
+    for (int i = str.length() - 1; i >= 0; i--)
+    {
+        if (str[i] == ' ')
+        {
+            //str.insert(i+1, "%20");
+            string temp;
+            temp = str.substr(i+1, str.length() - 1);
+            str.replace(i, str.length() - 1, "%20");
+            str.append(temp);
+        }
+    }
+}
