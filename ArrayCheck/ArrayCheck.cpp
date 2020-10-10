a8e9b200263de63fe998a7249b80a6996a39c265
HenryOrdin <HenryOrdin@163.com>
2020-10-10 15:33:44 +0800

    二分判断部分重复

--- ArrayCheck/ArrayCheck.cpp
+++ ArrayCheck/ArrayCheck.cpp
@@ -10,15 +10,21 @@ bool means_1(int Origin[],int OutArr[],int len);
 
 bool means_2(int Origin[], int len, int Duplication[]);
 
+bool means_3(int len, int Origin[]);
+int Bina_treat(int Origin[], int len, int Head, int End);
+
 int main()
 {
     std::cout << "Hello World!\n";
     int a[7] = { 1,1,5,5,6,4,3 };
     int b[7] = { 0 };
     int c[7] = {0};
+    int d[8] = { 1,2,3,3,4,5,6,2 };
     int len = sizeof(a)/sizeof(int);
+    int lend = sizeof(d) / sizeof(int);
     means_1(a, b,len);
     means_2(a, len, c);
+    means_3(lend, d);
 }
 
 bool means_1(int Origin[], int OutArr[],int len)
@@ -114,6 +120,61 @@ bool means_2(int Origin[], int len, int Duplication[])
     }
     cout << endl;
     return true;
+}
+
+//二分法
+bool means_3(int len, int Origin[])
+{
+    if (Origin == nullptr || sizeof(Origin) <= 0)
+    {
+        cout << "Null Array" << endl;
+        return false;
+    }
+
+    int start = 1;
+    int end = len - 1;
+    while (end >= start)
+    {
+        int middle = ((end - start) >> 1) + start;//等价static_cast<int>( (end-start-0.5)/2 )+start      向下取整 ,这里使用移位运算更简便
+        int count = Bina_treat(Origin, len, start, middle);
+
+        if (end == start)//重复校验
+        {
+            if (count > 1)
+            {
+                cout<<endl<< start;
+                break;
+            }
+            else
+                break;
+        }
+
+        //边界修改
+        if (count > (middle - start + 1))
+        {
+            end = middle;
+        }
+        else
+            start = middle + 1;
+    }
+
+    return -1;
+}
+int Bina_treat(int Origin[], int len, int Head, int End)
+{
+    if (Origin == nullptr)
+    {
+        return false;
+    }
+    int count = 0;
+    for (int i = 0; i < len; i++)
+    {
+        if (Origin[i] >= Head && Origin[i] <= End)
+        {
+            ++count;
+        }
+    }
+    return count;
 }
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
 // 调试程序: F5 或调试 >“开始调试”菜单
